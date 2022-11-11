93df6eb363be8cd2d715b0d25f338f5e91fc3bea
Stanislav <stas.lemeshonok98@mail.ru>
2022-11-11 13:23:58 +0300

    layout done

--- src/features/containers/LeaderBoardContainer/LeaderBoardContainer.jsx
+++ src/features/containers/LeaderBoardContainer/LeaderBoardContainer.jsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Header } from "../../components/Header/Header";
 import { RatingTable } from "../../components/RatingTable/RatingTable";
-import { Sidebar } from "../../components/Sidebar/Sidebar";
 import classes from './LeaderBoardContainer.module.css'
 
 export const LeaderBoardContainer = () => {
-    return <>
+    <div className={classes.body__wrappper}>
         <Header />
-        <div className={classes.body__wrappper}>
-            <Sidebar />
-            <RatingTable />
-        </div>
-    </>
+        <RatingTable />
+    </div>
 }
