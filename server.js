4805ecfec66ea695b623c7110d6ddab64b0c613a
Stanislav <stas.lemeshonok98@mail.ru>
2022-12-01 23:46:53 +0300

    create usercontroller

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 import express from 'express'
-import jwt from 'jsonwebtoken' //Для авторизации
 import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
 
-import { validationResult } from 'express-validator'
 import { registerValidation } from './src/server/validations/authValid.js'
 
-import UserModel from './src/server/models/User.js'
 import checkAuth from './src/server/utils/checkAuth.js'
+import * as UserController from './src/server/controllers/UserController.js'
 
 
 const app = express() //создание сервера
@@ -24,114 +21,10 @@ mongoose
   .catch((err) => console.log(err))
 
 
-app.post('/auth/login', async (req, res) => { //Вход для администратора
-  try {
-    const user = await UserModel.findOne({ email: req.body.email })
+app.post('/auth/register', registerValidation, UserController.register)
+app.post('/auth/login', UserController.login)
+app.get('/auth/me', checkAuth, UserController.getMe)
 
-    if (!user) {
-      return res.status(404).json({
-        message: 'Не удалось авторизоваться'
-      })
-    }
-
-    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
-
-    if (!isValidPass) {
-      return res.status(400).json({
-        message: 'Неверный логин или пароль'
-      })
-    }
-
-    const token = jwt.sign(  //если у пользователя есть токен, то этого хватит для дальнейших действий и выдачи прав
-      {
-        _id: user._id
-      },
-      'someSecret239',
-      {
-        expiresIn: '10y' //токен даётся на 10 лет
-      }
-    )
-
-    const { passwordHash, ...userData } = user._doc
-
-    res.json({
-      ...userData,
-      token
-    })
-
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({
-      message: 'Не удалось авторизоваться'
-    })
-  }
-})
-
-
-app.post('/auth/register', registerValidation, async (req, res) => { //Регистрация администраторов
-  try {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array())
-    }
-
-
-    const password = req.body.password
-    const salt = await bcrypt.genSalt(10)
-    const hash = await bcrypt.hash(password, salt) //Шифрование пароля
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      passwordHash: hash
-    })
-
-    const user = await doc.save()
-
-    const token = jwt.sign(  //если у пользователя есть токен, то этого хватит для дальнейших действий и выдачи прав
-      {
-        _id: user._id
-      },
-      'someSecret239',
-      {
-        expiresIn: '10y' //токен даётся на 10 лет
-      }
-    )
-
-    const { passwordHash, ...userData } = user._doc
-
-    res.json({
-      ...userData,
-      token
-    })
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({
-      message: 'Регистрация не удалась'
-    })
-  }
-})
-
-
-app.get('/auth/me', checkAuth, async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.userId)
-    if (!user) {
-      return res.status(404).json({
-        message: 'Пользователь не найден'
-      })
-    }
-
-    const { passwordHash, ...userData } = user._doc
-
-    res.json(userData)
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({
-      message: 'Нет доступа'
-    })
-  }
-})
 
 app.listen(PORT, 'localhost', (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`)
---
+++ src/server/controllers/UserController.js
@@ -0,0 +1,114 @@
+import jwt from 'jsonwebtoken' //Для авторизации
+import bcrypt from 'bcrypt'
+
+import { validationResult } from 'express-validator'
+
+import UserModel from '../models/User.js'
+
+
+export const register = async (req, res) => { //Регистрация администраторов
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array())
+    }
+
+
+    const password = req.body.password
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt) //Шифрование пароля
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      passwordHash: hash
+    })
+
+    const user = await doc.save()
+
+    const token = jwt.sign(  //если у пользователя есть токен, то этого хватит для дальнейших действий и выдачи прав
+      {
+        _id: user._id
+      },
+      'someSecret239',
+      {
+        expiresIn: '10y' //токен даётся на 10 лет
+      }
+    )
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json({
+      ...userData,
+      token
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Регистрация не удалась'
+    })
+  }
+}
+
+export const login = async (req, res) => { //Вход для администратора
+  try {
+    const user = await UserModel.findOne({ email: req.body.email })
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Не удалось авторизоваться'
+      })
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: 'Неверный логин или пароль'
+      })
+    }
+
+    const token = jwt.sign(  //если у пользователя есть токен, то этого хватит для дальнейших действий и выдачи прав
+      {
+        _id: user._id
+      },
+      'someSecret239',
+      {
+        expiresIn: '10y' //токен даётся на 10 лет
+      }
+    )
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json({
+      ...userData,
+      token
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Не удалось авторизоваться'
+    })
+  }
+}
+
+export const getMe = async (req, res) => { //Получение данных о себе
+  try {
+    const user = await UserModel.findById(req.userId)
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не найден'
+      })
+    }
+
+    const { passwordHash, ...userData } = user._doc
+
+    res.json(userData)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      message: 'Нет доступа'
+    })
+  }
+}
